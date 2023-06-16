c5de9789d7b8916d220e9f30559b10836fe7e6ff
karaaaaaaaaaa <71070624+karaaaaaaaaaa@users.noreply.github.com>
2023-06-16 20:41:54 +0300

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+Widget defuilttextform({
+  TextEditingController? controller,
+  required String lable,
+  IconData? prefixicon,
+  IconData? suffixicon,
+  Function()? suffixclick,
+  bool ispassword = false,
+  TextInputType keyboardType = TextInputType.emailAddress,
+  required dynamic validat,
+  required InputBorder Border,
+}) =>
+    TextFormField(
+      
+      validator: validat,
+      //  (value) {
+      //   if (value!.isEmpty) {
+      //     return "please enter your emails";
+      //   }
+      //   return null;
+    
+      // },
+      obscureText: ispassword,
+      keyboardType: keyboardType,
+      controller: controller,
+      decoration: InputDecoration(
+        
+          label: Text(lable),
+          prefixIcon: Icon(prefixicon),
+          suffixIcon: suffixicon != null
+              ? IconButton(
+                  onPressed: () {
+                    suffixclick!();
+                  },
+                  icon: Icon(suffixicon))
+              : null,
+          border: Border),
+    );
